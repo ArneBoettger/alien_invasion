a7305f3fae71a47657ff88df694531dd8f1aa5af
ArneBoettger <77060523+SeppReifen@users.noreply.github.com>
2021-06-29 13:47:44 +0200

    1

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    '''A class to store all settings for Alien Invasion'''
+
+    def __init__(self) -> None:
+        self.screen_width = 1000
+        self.screen_height = 600
+
+        #self.bg_color = (94, 4, 63) #uncomfortable purple
+        #self.bg_color = (25, 48, 33) #disgusting dark green
+        self.bg_color = (230, 230, 230)
